f99c802b911a637310f3697990dfc88559e96ca7
Atban <atbanbinaslam@gmail.com>
2022-06-08 03:02:40 +0500

    added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -110,7 +110,7 @@ class ProductList extends Component {
 
   constructor(renderHookId) {
     super(renderHookId);
-    this.fetchProducts();
+    this.fetchProducts(); //fetching 
   }
 
   fetchProducts() {
